521166783be324632c52c581b800cfb33b7af739
Denis Matveev <denis.vic.matveev@gmail.com>
2020-05-25 23:12:30 +0200

    Now tests run from the main class

--- Advanced Character SheetTests/Advanced_Character_SheetTests.swift
+++ Advanced Character SheetTests/Advanced_Character_SheetTests.swift
@@ -7,9 +7,11 @@
 //
 
 import XCTest
+@testable import Advanced_Character_Sheet
 
 class Advanced_Character_SheetTests: XCTestCase {
-
+    let compendium = Compendium()
+    
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
@@ -18,9 +20,19 @@ class Advanced_Character_SheetTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testAbilities() throws {
+        guard let abilities = compendium.abilities else {
+            return
+        }
+        
+        XCTAssertEqual(abilities.first!.name, "Strength")
+        XCTAssertEqual(abilities.first!.shortName, "STR")
+        XCTAssertEqual(abilities.last!.name, "Charisma")
+        XCTAssertEqual(abilities.last!.shortName, "CHA")
+        
+        var char = CharacterBuilder(abilities: abilities)
+        
+        XCTAssertNotNil(char.characterSheet)
     }
 
     func testPerformanceExample() throws {
