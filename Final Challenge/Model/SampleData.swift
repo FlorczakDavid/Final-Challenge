1535e3e9eb7389fdb0a0b9c9f01ee3831048b697
Denis Matveev <denis.vic.matveev@gmail.com>
2020-05-26 01:10:12 +0200

    Place for sample characters

---
+++ Final Challenge/Model/SampleData.swift
@@ -0,0 +1,36 @@
+//
+//  SampleData.swift
+//  Final Challenge
+//
+//  Created by Денис Матвеев on 26/05/2020.
+//  Copyright © 2020 david florczak. All rights reserved.
+//
+
+import Foundation
+
+// Place for sample characters
+struct Sample {
+    var compendium = Compendium()
+    
+    var denis: CharacterBuilder!
+    
+    init() {
+        guard let abilities = compendium.abilities else {
+            return
+        }
+        
+        denis = CharacterBuilder(abilities: abilities)
+        
+        denis.characterSheet.abilityScores[0].value = 8
+        denis.characterSheet.abilityScores[0].modifier = -1
+        denis.characterSheet.abilityScores[1].value = 10
+        denis.characterSheet.abilityScores[1].modifier = 0
+        denis.characterSheet.abilityScores[2].value = 16
+        denis.characterSheet.abilityScores[2].modifier = 3
+        denis.characterSheet.abilityScores[3].value = 18
+        denis.characterSheet.abilityScores[3].modifier = 4
+    }
+}
+
+
+
