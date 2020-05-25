b7f401abd9a9db8924a6ed3606c7b15262805b6f
Denis Matveev <denis.vic.matveev@gmail.com>
2020-05-26 00:38:53 +0200

    Compendium in unit tests. More improvement

--- Advanced Character SheetTests/CharacterSheetTests.swift
+++ Advanced Character SheetTests/CharacterSheetTests.swift
@@ -10,9 +10,9 @@
 import XCTest
 @testable import Advanced_Character_Sheet
 
-var sut: CharacterSheet! // System Under Test (SUT), or the object this test case class is concerned with testing.
-
 class CharacterSheetTests: XCTestCase {
+    
+    var sut: CharacterSheet! // System Under Test (SUT), or the object this test case class is concerned with testing.
     
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -22,13 +22,7 @@ class CharacterSheetTests: XCTestCase {
             return
         }
         
-        XCTAssertEqual(abilities.first!.name, "Strength")
-        XCTAssertEqual(abilities.first!.shortName, "STR")
-        XCTAssertEqual(abilities.last!.name, "Charisma")
-        XCTAssertEqual(abilities.last!.shortName, "CHA")
-        
-        let char = CharacterBuilder(abilities: abilities)
-        sut = char.characterSheet
+        sut = CharacterBuilder(abilities: abilities).characterSheet
     }
 
     override func tearDownWithError() throws {
@@ -37,15 +31,18 @@ class CharacterSheetTests: XCTestCase {
         // Cleaning up
         sut = nil
     }
-
-    func testAbilities() throws {
+    
+    func testCSExists() throws {
+        XCTAssertNotNil(sut)
+    }
+    
+    func testAbilityRolls() throws {
         // It’s good practice to format the test into given, when and then sections:
         // Given: Here, you set up any values needed. In this example, you create a guess value so you can specify how much it differs from targetValue.
         // When: In this section, you’ll execute the code being tested: Call check(guess:).
         // Then: This is the section where you’ll assert the result you expect with a message that prints if the test fails. In this case, sut.scoreRound should equal 95 (100 – 5).
-        XCTAssertNotNil(sut)
-        XCTAssertLessThan(sut.abilityScores.first!.roll().result, 20 + sut.abilityScores.first!.modifier)
-        XCTAssertGreaterThan(sut.abilityScores.first!.roll().result, 0)
+        XCTAssertLessThan(sut.abilityScores.first!.roll().result, 20 + sut.abilityScores.first!.modifier, "Ability roll is out of range")
+        XCTAssertGreaterThan(sut.abilityScores.first!.roll().result, 0, "Ability roll is out of range")
     }
 
     func testPerformanceExample() throws {
---
+++ Advanced Character SheetTests/CompendiumTests.swift
@@ -0,0 +1,34 @@
+//
+//  CompendiumTests.swift
+//  Advanced Character SheetTests
+//
+//  Created by Денис Матвеев on 26/05/2020.
+//  Copyright © 2020 david florczak. All rights reserved.
+//
+
+import XCTest
+@testable import Advanced_Character_Sheet
+
+class CompendiumTests: XCTestCase {
+    
+    var sut: Compendium!
+    
+    override func setUpWithError() throws {
+        sut = Compendium()
+    }
+
+    override func tearDownWithError() throws {
+        sut = nil
+    }
+
+    func testAbilitiesAreCorrect() throws {
+        guard let abilities = sut.abilities else {
+            return
+        }
+        
+        XCTAssertEqual(abilities.first!.name, "Strength")
+        XCTAssertEqual(abilities.first!.shortName, "STR")
+        XCTAssertEqual(abilities.last!.name, "Charisma")
+        XCTAssertEqual(abilities.last!.shortName, "CHA")
+    }
+}
